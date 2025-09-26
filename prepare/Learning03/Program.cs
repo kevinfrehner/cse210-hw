c0cbffec7684575983ae6dd8ae876716cccd312f
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-09-26 00:34:15 -0600

    Update Program.cs

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int answer = -1;
+
+        while (answer != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            answer = int.Parse(Console.ReadLine());
+
+            if (magicNumber > answer)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < answer)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
+
+
+
 }
+
