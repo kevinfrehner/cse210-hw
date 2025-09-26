fe08873150fd5abf01282bdf181003b807178ecb
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-09-26 00:34:59 -0600

    Update Program.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,56 @@ using System;
 
 class Program
 {
+    using System;
+
+class Program
+{
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        ShowGreeting();
+
+        string user = GetUserName();
+        int favoriteNum = GetUserFavoriteNumber();
+
+        int squaredValue = CalculateSquare(favoriteNum);
+
+        int yearOfBirth;
+        GetBirthYear(out yearOfBirth);
+
+        PresentResults(user, squaredValue, yearOfBirth);
+    }
+
+    static void ShowGreeting()
+    {
+        Console.WriteLine("Hello! Thanks for running this program.");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int GetUserFavoriteNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static void GetBirthYear(out int birthYear)
+    {
+        Console.Write("What year were you born? ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int CalculateSquare(int num)
+    {
+        return num * num;
+    }
+
+    static void PresentResults(string userName, int squaredNum, int birthYear)
+    {
+        Console.WriteLine($"{userName}, your number squared is {squaredNum}.");
+        Console.WriteLine($"{userName}, you will be {2025 - birthYear} years old this year.");
     }
 }
