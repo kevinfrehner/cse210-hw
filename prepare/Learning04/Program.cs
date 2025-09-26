7574a223310da07d45f7cf97fb97d32b228cab14
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-09-26 00:34:33 -0600

    Update Program.cs

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        List<int> numbersList = new List<int>();
+
+        int Number = -1;
+        while (Number != 0)
+        {
+            Console.Write("Enter a list of numbers, type 0 when finished. ");
+            string response = Console.ReadLine();
+            Number = int.Parse(response);
+            if (Number != 0)
+            {
+                numbersList.Add(Number);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+         {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+      
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+       
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
+    
