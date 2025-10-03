5a75a523e8f43711395bf7969c1960315892974f
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-10-02 20:28:37 -0600

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
     }
-}
+    public class Job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+        }
+    }
+    public class Resume
+    {
+        public string _name;
+        public List<Job> _jobs = new List<Job>();
+
+        public Resume()
+        {
+            _jobs.Add(new Job { _jobTitle = "Dishwasher", _company = "Cubby's Pizza", _startYear = 2022, _endYear = 2023 });
+            _jobs.Add(new Job { _jobTitle = "Baker", _company = "Crumbl", _startYear = 2023, _endYear = 2023 });
+            _jobs.Add(new Job { _jobTitle = "Physical Therapy Aide", _company = "BSMART", _startYear = 2025, _endYear = 2025  });
+            _jobs.Add(new Job { _jobTitle = "Dishwasher", _company = "University Food Services", _startYear = 2025, _endYear = 2027});
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+
+            foreach (Job job in _jobs)
+            {
+                job.Display();
+            }
+        }
+    }
+        
+
+    
+}   
