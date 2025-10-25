694c547c05ae8d9998624ed3e6c71b9275032dd3
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-10-25 17:09:57 -0600

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture scripture = new Scripture();
+        scripture.DisplayScripture();
+
+        
+        while (true)
+        {
+            Console.WriteLine("Please press 'enter' to continue the program, or, type 'quit' to exit the program");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "")
+            {
+                scripture.PopFunction();
+                Console.Clear();
+                scripture.DisplayScripture();
+            }
+            else if (userInput.ToLower() == "quit")
+            {
+                Console.WriteLine("Quitting Program.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
     }
 }
