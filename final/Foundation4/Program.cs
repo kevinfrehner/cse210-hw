2b72e1ddcc8505035995a33f40581040fe1cff5e
kevinfrehner <168138751+kevinfrehner@users.noreply.github.com>
2025-12-13 16:23:52 -0700

    Update Program.cs

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>()
+        {
+            new Running("03 Nov 2022", 30, 3.0), 
+            new Cycling("03 Nov 2022", 40, 15), 
+            new Swimming("03 Nov 2022", 25, 30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
